df9b52ca47a70b05d75323dd9ca6ff18b738d608
Ivan Nazarov <ivannz@yandex.ru>
2015-02-06 10:59:19 +0300

    Minor edits

--- year_14_15/course_project/code/python/hsssi_processes.py
+++ year_14_15/course_project/code/python/hsssi_processes.py
@@ -38,26 +38,3 @@ def Hermite4( N, H = 0.9, seed = None, downsample = 16 ) :
 
 def Weier( N, H = 0.7, deterministic = False, seed = None ) :
 	return synthweier( N, H = H, nu0 = 1.2, nue = 1000, seed = seed )[0]
-
-
-import matplotlib.pyplot as plt
-N = 2**15
-t = np.arange( N, dtype = np.float ) / ( N - 1 )
-
-a1 = fBM( N, 0.6 )
-a2 = Rosenblatt( N, 0.6, downsample = 16 )
-a3 = Hermite3( N, 0.9, downsample = 16 )
-a4 = Hermite4( N, 0.925, downsample = 16 )
-a5 = Weier( N, 0.6 )
-
-plt.figure( figsize = (10,6) )
-plt.subplot(211)
-plt.plot(t, a1, "r-", linewidth = 1 )
-plt.plot(t, a5, "b-", linewidth = 1 )
-
-plt.subplot(212)
-plt.plot(t, a2, "r-", linewidth = 1 )
-plt.plot(t, a3, "k-", linewidth = 1 )
-plt.plot(t, a4, "b-", linewidth = 1 )
-
-plt.show( )
