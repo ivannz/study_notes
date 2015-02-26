b8c3bf4bc2327732d20fa19e54d70cbf02aa0902
Ivan Nazarov <ivannz@yandex.ru>
2015-02-26 12:34:28 +0300

    Added proper random number generator initialization

--- year_14_15/course_project/code/python/monte_carlo.py
+++ year_14_15/course_project/code/python/monte_carlo.py
@@ -8,6 +8,8 @@ import numpy as np
 import IPython.parallel as mp
 from IPython.parallel import interactive
 
+import struct as struct
+
 def monte_carlo_serial( generator, kernel, M = 100, quiet = False, **kwargs ) :
 ## Return a MxKxT 3D array made from M concatenated 2D KxT slices
 	import time
@@ -44,7 +46,7 @@ def __mp_mc_worker( ) :
 ##  as to avoid rndom number sequence collisions on different worker
 ##  units. cf. http://ieeexplore.ieee.org/xpl/articleDetails.jsp?arnumber=5547156
 ##  http://stackoverflow.com/questions/12915177/same-output-in-different-workers-in-multiprocessing
-	np.random.seed( )
+	np.random.seed( seed = seed )
 ## Each process generator must be equipped with a reset method() which
 ##  resets its internal state.
 	generator.reset( )
@@ -68,6 +70,20 @@ def __mp_mc_setup( generator, kernel, M = 100, **kwargs ) :
 	clu.execute( 'from hsssi_processes import synth_Rosenblatt, synth_Hermite3, synth_Hermite4', block = True )
 # Distribute the workload evenly among the members of the cluster
 	clu.scatter( 'local_replications', range( M ), block = True )
+## The main problem is that it is possible that each worker reads
+##  its seed from the same source and at the same time, which would
+##  produces dangerously correlated results, even meaningless. Therefore
+##  before starting the kernels, let the parent process generate
+##  some entropy for each child process.
+##  http://stackoverflow.com/questions/2396209/best-seed-for-parallel-process
+## Generate 32bit values uniformly at random.
+	seeds = np.random.randint( 0xFFFFFFFF, size = len( cli ) )
+## Alternative way for UNIX systems is to read from /dev/random.
+##	with open( "/dev/random", "rb" ) as dev :
+##		seeds = [ struct.unpack( 'I', dev.read( 4 ) )[ 0 ] for c in cli ]
+## Dispatch individual seed values to each worker.
+	for c, seed in zip( cli, seeds ) :
+		c.push( { 'seed' : seed } )
 ## Pass the necesary environment to the cluster.
 	clu.push({ 'generator' : generator, 'local_kwargs' : kwargs, 'local_M': M,
 		'kernel': interactive( kernel ) }, block = True )
